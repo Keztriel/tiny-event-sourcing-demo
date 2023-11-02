e40cb2101290053900e8a1e632c608e3e522d839
Keztriel <sueten@inbox.ru>
2023-11-02 20:38:17 +0300

    add mappings for ProjectController.kt

--- src/main/kotlin/ru/quipy/controller/ProjectController.kt
+++ src/main/kotlin/ru/quipy/controller/ProjectController.kt
@@ -1,18 +1,15 @@
 package ru.quipy.controller
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import ru.quipy.api.ProjectAggregate
-import ru.quipy.api.ProjectCreatedEvent
-import ru.quipy.api.TaskCreatedEvent
+import ru.quipy.api.*
 import ru.quipy.core.EventSourcingService
-import ru.quipy.logic.ProjectAggregateState
-import ru.quipy.logic.addTask
-import ru.quipy.logic.create
+import ru.quipy.logic.*
 import java.util.*
 
 @RestController
@@ -26,7 +23,10 @@ class ProjectController(
         return projectEsService.create { it.create(UUID.randomUUID(), projectTitle, creatorId) }
     }
 
-//    @PostMapping()
+    @PostMapping("/{projectId}/rename")
+    fun renameProject(@PathVariable projectId: UUID, @RequestParam name: String, @RequestParam editorId: String): ProjectRenamedEvent {
+        return projectEsService.update(projectId) { it.rename(name, editorId) }
+    }
 
     @GetMapping("/{projectId}")
     fun getProject(@PathVariable projectId: UUID) : ProjectAggregateState? {
@@ -40,5 +40,33 @@ class ProjectController(
         }
     }
 
+    @DeleteMapping("/{projectId}/tasks/{taskId}")
+    fun deleteTask(@PathVariable projectId: UUID, @PathVariable taskId: UUID) : TaskDeletedEvent {
+        return projectEsService.update(projectId) { it.removeTask(taskId) }
+    }
 
+    @PostMapping("/{projectId}/tasks/{taskId}/executors/{executorId}")
+    fun assignTaskToExecutor(@PathVariable projectId: UUID, @PathVariable taskId: UUID, @PathVariable executorId: UUID)
+    : TaskAssignedEvent {
+        return projectEsService.update(projectId) { it.assignTaskToExecutor(taskId, executorId) }
+    }
+
+    @DeleteMapping("/{projectId}/tasks/{taskId}/executors/{executorId}")
+    fun unassignTaskToExecutor(@PathVariable projectId: UUID, @PathVariable taskId: UUID, @PathVariable executorId: UUID)
+    : TaskUnassignedEvent {
+        return projectEsService.update(projectId) {it.unassignTaskToExecutor(taskId, executorId)}
+    }
+
+    @PostMapping("/{projectId}/tags/{tagName}")
+    fun createTag(@PathVariable projectId: UUID, @PathVariable tagName: String) : TagCreatedEvent {
+        return projectEsService.update(projectId) { it.createTag(tagName) }
+    }
+
+    @PostMapping("/{projectId}/tasks/{taskId}/tags/{tagId}")
+    fun assignTagToTask(@PathVariable projectId: UUID, @PathVariable taskId: UUID, @PathVariable tagId: UUID)
+    : TagAssignedToTaskEvent {
+        return projectEsService.update(projectId) {
+            it.assignTagToTask(tagId, taskId)
+        }
+    }
 }
