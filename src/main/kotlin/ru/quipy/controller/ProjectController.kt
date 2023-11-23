ed444085e32604042701d61164aee69ae6772feb
Keztriel <sueten@inbox.ru>
2023-11-23 19:29:03 +0300

    add mappings for ProjectController.kt

--- src/main/kotlin/ru/quipy/controller/ProjectController.kt
+++ src/main/kotlin/ru/quipy/controller/ProjectController.kt
@@ -1,12 +1,6 @@
 package ru.quipy.controller
 
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import ru.quipy.api.*
 import ru.quipy.core.EventSourcingService
 import ru.quipy.logic.*
@@ -18,12 +12,17 @@ class ProjectController(
     val projectEsService: EventSourcingService<UUID, ProjectAggregate, ProjectAggregateState>
 ) {
 
+//    @GetMapping()
+//    fun getAllProjects() : List<ProjectAggregateState>{
+////        return projectEsService
+//    }
+
     @PostMapping("/{projectTitle}")
     fun createProject(@PathVariable projectTitle: String, @RequestParam creatorId: String) : ProjectCreatedEvent {
         return projectEsService.create { it.create(UUID.randomUUID(), projectTitle, creatorId) }
     }
 
-    @PostMapping("/{projectId}/rename")
+    @PatchMapping("/{projectId}/rename")
     fun renameProject(@PathVariable projectId: UUID, @RequestParam name: String, @RequestParam editorId: String): ProjectRenamedEvent {
         return projectEsService.update(projectId) { it.rename(name, editorId) }
     }
